9f855f102c1cb90a7d3e2badd98edb2cd26bfb34
Mathieu PATUREL <australie.p@gmail.com>
2018-04-27 15:01:51 +1000

    :art: @ utils.gui

--- utils/gui.py
+++ utils/gui.py
@@ -47,62 +47,6 @@ class Options(dict):
     def __setattr__(self, attr, value):
         self[attr] = value
 
-class TextBox:
-
-    def __init__(self, font, user_opt={}):
-        opt = Options()
-        opt.initialtext = ""
-        opt.bordercolor = 33, 33, 33
-        opt.focusedbordercolor = 150, 150, 150
-        opt.fgcolor = 150, 150, 150
-        opt.bgcolor = None
-        opt.margin = 10, 10
-        opt.style = pygame.freetype.STYLE_DEFAULT
-        opt.fontsize = 20
-        opt.size = 200, font.get_sized_height(getattr(user_opt, 'fontsize',
-                                                      opt.fontsize)) + 10
-        opt.thickness = 1
-        opt.update(user_opt)
-
-        self.opt = opt
-
-        self.text = self.opt.initialtext
-        self.font = font
-        self.focused = False
-
-    def event(self, e):
-        if not self.focused:
-            return
-        if e.type == KEYDOWN:
-            if e.unicode == '\x08':
-                if len(self.text) > 0:
-                    self.text = self.text[:-1]
-            elif e.unicode == '\r':
-                return True
-            else:
-                self.text += e.unicode
-
-    def render(self):
-        surface = pygame.Surface((self.opt.size[0] + self.opt.thickness,
-                                 self.opt.size[1] + self.opt.thickness))
-        rect = surface.get_rect()
-        bordercolor = self.opt.bordercolor if self.focused \
-                      else self.opt.focusedbordercolor
-        pygame.draw.rect(surface, bordercolor, rect.inflate(-self.opt.thickness,
-            -self.opt.thickness), self.opt.thickness)
-        origin = (rect.left + self.opt.margin[0],
-                 rect.bottom - self.opt.margin[1])
-        self.font.render_to(surface, origin, self.text,
-                            self.opt.fgcolor, self.opt.bgcolor, self.opt.style,
-                            size=self.opt.fontsize)
-        return surface, rect
-
-    def __str__(self):
-        return '<TextBox text={!r}>'.format(self.text)
-
-    def __repr__(self):
-        return self.__str__()
-
 class Button(pygame.sprite.Sprite):
 
     """A simple button
@@ -163,6 +107,61 @@ class Button(pygame.sprite.Sprite):
     def __repr__(self):
         return self.__str__()
 
+class TextBox:
+
+    def __init__(self, font, user_opt={}):
+        opt = Options()
+        opt.initialtext = ""
+        opt.bordercolor = 33, 33, 33
+        opt.focusedbordercolor = 150, 150, 150
+        opt.fgcolor = 150, 150, 150
+        opt.bgcolor = None
+        opt.margin = 10, 10
+        opt.style = pygame.freetype.STYLE_DEFAULT
+        opt.fontsize = 20
+        opt.size = 200, font.get_sized_height(getattr(user_opt, 'fontsize',
+                                                      opt.fontsize)) + 10
+        opt.thickness = 1
+        opt.update(user_opt)
+
+        self.opt = opt
+
+        self.text = self.opt.initialtext
+        self.font = font
+        self.focused = False
+
+    def event(self, e):
+        if not self.focused:
+            return
+        if e.type == KEYDOWN:
+            if e.unicode == '\x08':
+                if len(self.text) > 0:
+                    self.text = self.text[:-1]
+            elif e.unicode == '\r':
+                return True
+            else:
+                self.text += e.unicode
+
+    def render(self):
+        surface = pygame.Surface((self.opt.size[0] + self.opt.thickness,
+                                 self.opt.size[1] + self.opt.thickness))
+        rect = surface.get_rect()
+        bordercolor = self.opt.bordercolor if self.focused \
+                      else self.opt.focusedbordercolor
+        pygame.draw.rect(surface, bordercolor, rect.inflate(-self.opt.thickness,
+            -self.opt.thickness), self.opt.thickness)
+        origin = (rect.left + self.opt.margin[0],
+                 rect.bottom - self.opt.margin[1])
+        self.font.render_to(surface, origin, self.text,
+                            self.opt.fgcolor, self.opt.bgcolor, self.opt.style,
+                            size=self.opt.fontsize)
+        return surface, rect
+
+    def __str__(self):
+        return '<TextBox text={!r}>'.format(self.text)
+
+    def __repr__(self):
+        return self.__str__()
 
 class MessageBox:
 
