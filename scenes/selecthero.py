0de91eae3e4be0720dd7985cc36e99062426d8de
Mathieu PATUREL <australie.p@gmail.com>
2018-04-28 16:33:09 +1000

    :construction: highlight hero card working

--- scenes/selecthero.py
+++ scenes/selecthero.py
@@ -48,7 +48,7 @@ class SelectHero:
                 fgcolor=TEXT_FG, bgcolor=None)
             surface.blit(paddedsurface, (10, r.bottom + 10))
             pygame.draw.rect(surface, (33, 33, 33), cardrect, 1)
-            self.cards.append(surface)
+            self.cards.append((surface, cardrect.copy()))
 
         self.allcards = pygame.Surface((cardrect.width * 2 + margin,
             int(len(self.cards) / 2 + .5) * (cardrect.height + margin) - margin))
@@ -59,27 +59,52 @@ class SelectHero:
         self.originaltop = self.allcardsrect.top
 
         top = 0
-        for i, s in enumerate(self.cards):
-            left = 0
+        for i, (s, r) in enumerate(self.cards):
             if i % 2 == 1:
-                left += cardrect.width + 20
-            self.allcards.blit(s, (left, top))
+                r.left = cardrect.width + 20
+            r.top = top
+            self.allcards.blit(s, r)
             if i % 2 == 1:
                 top += cardrect.height + 20
 
         self.clip = self.allcardsrect.clip(self.m.rect)
+        self.hovered = None
 
     async def event(self, e):
-        if e.type == MOUSEBUTTONDOWN and self.clip.collidepoint(e.pos):
+        # we check only clicks in the hero selector, and we shouldn't
+        # be able to click hero that aren't in clipped area (that we already
+        # scrolled)
+        if e.type not in (MOUSEMOTION, MOUSEBUTTONDOWN):
+            return
+        if not self.clip.collidepoint(e.pos):
+            self.hovered = None
+            return
+        checkcollide = False
+        if e.type == MOUSEBUTTONDOWN:
             if e.button == 5 \
                     and self.allcardsrect.top < self.originaltop:
                 self.allcardsrect.top += self.scrollspeed
+                if self.hovered:
+                    self.hovered.top += self.scrollspeed
+                checkcollide = True
             elif e.button == 4 \
                     and self.allcardsrect.bottom > self.m.rect.bottom:
                 self.allcardsrect.top -= self.scrollspeed
+                if self.hovered:
+                    self.hovered.top -= self.scrollspeed
+                checkcollide = True
+
+        if e.type == MOUSEMOTION or checkcollide:
+            self.hovered = None
+            for _, r in self.cards:
+                r = r.move(self.allcardsrect.topleft)
+                if r.collidepoint(e.pos):
+                    self.hovered = r
 
     async def render(self):
         self.m.screen.blit(self.title, self.titlerect)
         self.m.screen.set_clip(self.clip)
         self.m.screen.blit(self.allcards, self.allcardsrect)
+        if self.hovered:
+            pygame.draw.rect(self.m.screen, pygame.Color('red'), self.hovered, 1)
         self.m.screen.set_clip(None)
